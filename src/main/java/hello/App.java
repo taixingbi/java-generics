172ed24dc476368738c5bbebe6a4e2cd04bff31e
taixingbi <bitaihang@gmail.com>
2021-10-01 15:17:25 -0400

    add compare

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -21,6 +21,10 @@ public class App {
         Integer[] intArray = { 1, 2, 3, 4, 5 };
         printArray(intArray);
 
+        // ---------------------------------------
+        System.out.printf("Max of %d, %d and %d is %d\n\n",
+                3, 4, 5, maximum( 3, 4, 5 ));
+
         System.out.println("----------app end----------");
     }
 
@@ -33,5 +37,18 @@ public class App {
             System.out.printf("%s ", element);
         }
         System.out.println();
+    }
+
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x;   // assume x is initially the largest
+
+        if(y.compareTo(max) > 0) {
+            max = y;   // y is the largest so far
+        }
+
+        if(z.compareTo(max) > 0) {
+            max = z;   // z is the largest now
+        }
+        return max;   // returns the largest object
     }
 }
